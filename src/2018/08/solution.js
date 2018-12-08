a923164b155bf93c885fd712d4b956fe8235692a
Brendan Moore <brendan.moore@rangle.io>
2018-12-08 01:26:06 -0500

    day 8 pt2

--- src/2018/08/solution.js
+++ src/2018/08/solution.js
@@ -19,8 +19,9 @@ const getChild = () => {
   const children = getChildren(header.children);
   const metadata = getMetadata(header.metadata);
   const tempSum = sumArrayNum(metadata);
+  const value = getValue(children, metadata);
   metaSum += tempSum;
-  return { header, children, metadata };
+  return { header, children, metadata, value };
 };
 
 const getMetadata = (numMetadata) => sequence.splice(0, numMetadata);
@@ -33,7 +34,15 @@ const getChildren = (numChildren) => {
     }
   }
   return children;
-}; 
+};
+
+const getValue = (children, metadata) => {
+  if (children.length === 0) return sumArrayNum(metadata);
+  return metadata.reduce(
+   (acc, cur) => acc + ((children[cur-1] && children[cur-1].value) || 0),
+   0
+  );
+};
 
 const tree = getChild();
 
